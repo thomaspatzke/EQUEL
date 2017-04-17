7aaacfe658eb1f1e3b02b0cf46de5269a8bf6f9b
Thomas Patzke <thomas@patzke.org>
2017-04-18 00:13:00 +0200

    Aggregation order

--- equel/engine.py
+++ equel/engine.py
@@ -25,7 +25,7 @@ class EQUELEngine:
             (PT_SEARCH, ["nest"], search.NestQueryPlugin),
             (PT_SEARCH, ["script"], search.ScriptQueryPlugin),
             (PT_SEARCH, ["scriptfield"], search.ScriptFieldPlugin),
-            (PT_AGGREGATE, ["fallback"], generic.GenericPlugin),
+            (PT_AGGREGATE, ["fallback"], aggregate.GenericAggregationPlugin),
             (PT_AGGREGATE, ["shortcut"], aggregate.AggregationShortcutPlugin),
             (PT_AGGREGATE, ["groupby", "add_sum", "add_min", "add_max", "valuecount"], aggregate.AggregationKeywordsPlugin),
             (PT_OUTPUT, ["plain"], output.BaseOutputPlugin),
--- equel/plugins/aggregate.py
+++ equel/plugins/aggregate.py
@@ -1,6 +1,27 @@
 # Aggregation Plugins
 from .generic import BasePlugin, GenericPlugin, BaseShortcutPlugin, EQUELPluginException
 
+class GenericAggregationPlugin(GenericPlugin):
+    """Extends generic EQUEL to JSON conversion by aggregation-specific aspects"""
+    name = "Generic Aggregation Plugin"
+    description = "Generic EQUEL aggregation to JSON conversion"
+
+    def apply(self, verb, *args, **kwargs):
+         res = super().apply(verb, *args, **kwargs)
+         try:       # convert order parameter into ES order JSON
+             order = res[verb]["order"]
+             direction = "asc"
+             if order[-1] == "-":
+                 order = order[:-1]
+                 direction = "desc"
+             elif order[-1] == "+":
+                 order = order[:-1]
+                 direction = "asc"
+             res[verb]["order"] = { order: direction }
+         except KeyError:   # no order given
+             pass
+         return res
+
 class AggregationShortcutPlugin(BaseShortcutPlugin):
     """
     Shortcuts to commonly used aggregations.
